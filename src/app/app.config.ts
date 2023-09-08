bb0832cf50ceabe6f1bd44c4f2b22df55e3e0075
shaonkhansur <shaon@npcoding.com>
2023-09-08 23:31:27 +0600

    firebase initiated

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,40 +1,60 @@
 import { provideHttpClient } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { LuxonDateAdapter } from '@angular/material-luxon-adapter';
 import { DateAdapter, MAT_DATE_FORMATS } from '@angular/material/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { PreloadAllModules, provideRouter, withInMemoryScrolling, withPreloading } from '@angular/router';
+import {
+    PreloadAllModules,
+    provideRouter,
+    withInMemoryScrolling,
+    withPreloading,
+} from '@angular/router';
 import { provideFuse } from '@fuse';
 import { appRoutes } from 'app/app.routes';
-import { provideAuth } from 'app/core/auth/auth.provider';
+import { provideAuthFuse } from 'app/core/auth/auth.provider';
 import { provideIcons } from 'app/core/icons/icons.provider';
 import { provideTransloco } from 'app/core/transloco/transloco.provider';
 import { mockApiServices } from 'app/mock-api';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore} from '@angular/fire/firestore';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { environment } from 'environments/environment';
+
 
 export const appConfig: ApplicationConfig = {
     providers: [
+        //Firebase config
+        importProvidersFrom(
+            provideFirebaseApp(() => initializeApp(environment.firebase)),
+            provideFirestore(() => getFirestore()),
+            provideAuth(() => getAuth()),
+            provideStorage(() => getStorage())
+        ),
+
         provideAnimations(),
         provideHttpClient(),
-        provideRouter(appRoutes,
+        provideRouter(
+            appRoutes,
             withPreloading(PreloadAllModules),
-            withInMemoryScrolling({scrollPositionRestoration: 'enabled'}),
+            withInMemoryScrolling({ scrollPositionRestoration: 'enabled' })
         ),
 
         // Material Date Adapter
         {
-            provide : DateAdapter,
+            provide: DateAdapter,
             useClass: LuxonDateAdapter,
         },
         {
-            provide : MAT_DATE_FORMATS,
+            provide: MAT_DATE_FORMATS,
             useValue: {
-                parse  : {
+                parse: {
                     dateInput: 'D',
                 },
                 display: {
-                    dateInput         : 'DDD',
-                    monthYearLabel    : 'LLL yyyy',
-                    dateA11yLabel     : 'DD',
+                    dateInput: 'DDD',
+                    monthYearLabel: 'LLL yyyy',
+                    dateA11yLabel: 'DD',
                     monthYearA11yLabel: 'LLLL yyyy',
                 },
             },
@@ -44,46 +64,46 @@ export const appConfig: ApplicationConfig = {
         provideTransloco(),
 
         // Fuse
-        provideAuth(),
+        provideAuthFuse(),
         provideIcons(),
         provideFuse({
             mockApi: {
-                delay   : 0,
+                delay: 0,
                 services: mockApiServices,
             },
-            fuse   : {
-                layout : 'classy',
-                scheme : 'light',
+            fuse: {
+                layout: 'classy',
+                scheme: 'light',
                 screens: {
                     sm: '600px',
                     md: '960px',
                     lg: '1280px',
                     xl: '1440px',
                 },
-                theme  : 'theme-default',
-                themes : [
+                theme: 'theme-default',
+                themes: [
                     {
-                        id  : 'theme-default',
+                        id: 'theme-default',
                         name: 'Default',
                     },
                     {
-                        id  : 'theme-brand',
+                        id: 'theme-brand',
                         name: 'Brand',
                     },
                     {
-                        id  : 'theme-teal',
+                        id: 'theme-teal',
                         name: 'Teal',
                     },
                     {
-                        id  : 'theme-rose',
+                        id: 'theme-rose',
                         name: 'Rose',
                     },
                     {
-                        id  : 'theme-purple',
+                        id: 'theme-purple',
                         name: 'Purple',
                     },
                     {
-                        id  : 'theme-amber',
+                        id: 'theme-amber',
                         name: 'Amber',
                     },
                 ],
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+export const environment = {
+    production: false,
+    // baseUrl: "http://127.0.0.1:5001/revivo-5afd9/us-central1/app/api/",
+    baseUrl: 'https://app-sggdm7rn4q-uc.a.run.app/api/',
+    externalUrl: 'https://revivo-group.top-agt.net/api/',
+    firebase: {
+        projectId: 'revivo-5afd9',
+        appId: '1:561970817375:web:f74dd47bac20e041aeaeea',
+        databaseURL: 'https://revivo-5afd9-default-rtdb.firebaseio.com',
+        storageBucket: 'revivo-5afd9.appspot.com',
+        locationId: 'us-central',
+        apiKey: 'AIzaSyAnrz9EqB_zH8jGt16vYBa1-Mh9a4K5Yv0',
+        authDomain: 'revivo-5afd9.firebaseapp.com',
+        messagingSenderId: '561970817375',
+        measurementId: 'G-7RLMVYDNEG',
+    },
+};
