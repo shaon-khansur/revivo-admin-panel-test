8967544e32bf7abe826f39b5e64897952af46f36
Md. Asif Ullah Sikder <asifsikder23@gmail.com>
2023-12-17 20:12:54 +0600

    edit and add field

--- src/app/modules/admin/deals/destination/add-destination/add-destination.component.ts
+++ src/app/modules/admin/deals/destination/add-destination/add-destination.component.ts
@@ -1,4 +1,10 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+    Component,
+    ElementRef,
+    OnInit,
+    ViewChild,
+    inject,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
@@ -15,129 +21,199 @@ import {
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import {
+    MatChipEditedEvent,
+    MatChipInputEvent,
+    MatChipsModule,
+} from '@angular/material/chips';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+
 import { DestinationService } from '../service/destination.service';
 import {
-  getStorage,
-  ref,
-  uploadBytes,
-  uploadBytesResumable,
-  getDownloadURL
+    getStorage,
+    ref,
+    uploadBytes,
+    uploadBytesResumable,
+    getDownloadURL,
 } from '@angular/fire/storage';
-import { initializeApp } from 'firebase/app'
+import { initializeApp } from 'firebase/app';
 import { environment } from 'environments/environment';
+import { LiveAnnouncer } from '@angular/cdk/a11y';
 
 const fapp = initializeApp(environment.firebase);
 
+export interface Airports {
+    name: string;
+}
+
 @Component({
-  selector: 'app-add-destination',
-  standalone: true,
-  imports: [
-      CommonModule,
-      MatDialogModule,
-      MatButtonModule,
-      MatIconModule,
-      MatFormFieldModule,
-      FormsModule,
-      ReactiveFormsModule,
-      MatSelectModule,
-      MatInputModule,
-      MatCheckboxModule,
-      MatGridListModule
-  ],
-  templateUrl: './add-destination.component.html',
-  styleUrls: ['./add-destination.component.scss']
+    selector: 'app-add-destination',
+    standalone: true,
+    imports: [
+        CommonModule,
+        MatDialogModule,
+        MatButtonModule,
+        MatIconModule,
+        MatFormFieldModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatSelectModule,
+        MatInputModule,
+        MatCheckboxModule,
+        MatGridListModule,
+        MatChipsModule,
+    ],
+    templateUrl: './add-destination.component.html',
+    styleUrls: ['./add-destination.component.scss'],
 })
 export class AddDestinationComponent implements OnInit {
-  @ViewChild('avatarFileInput') private _avatarFileInput: ElementRef;
-  form: FormGroup;
-  constructor(
-      private fb: FormBuilder,
-      private destinationService: DestinationService,
-      private _dialogRef: MatDialogRef<AddDestinationComponent>
-  ) {}
+    @ViewChild('avatarFileInput') private _avatarFileInput: ElementRef;
+    form: FormGroup;
+    separatorKeysCodes: number[] = [ENTER, COMMA];
+    addOnBlur = true;
+    announcer = inject(LiveAnnouncer);
+    airportChips: string[] = [];
+    CityNames: string[] = [];
 
-  ngOnInit(): void {
-    this.form = this.fb.group({
-      name: [null, Validators.required],
-      avatar: [null],
-      index: [null, [Validators.required, Validators.pattern(/^[0-9]+$/)]],
-      airport: [null, Validators.required],
-      description: [null, Validators.required],
-      active: [false]
-    });
-    
-  }
+    constructor(
+        private fb: FormBuilder,
+        private destinationService: DestinationService,
+        private _dialogRef: MatDialogRef<AddDestinationComponent>
+    ) {}
 
-  uploadAvatar(fileList: FileList): void {
-    // Return if canceled
-    if (!fileList.length) {
-      return;
+    ngOnInit(): void {
+        this.form = this.fb.group({
+            name: [null, Validators.required],
+            destinationId: [null],
+            hotelStar: [null],
+            avatar: [null],
+            index: [
+                null,
+                [Validators.required, Validators.pattern(/^[0-9]+$/)],
+            ],
+            airport: [null, Validators.required],
+            airportCode: [null, Validators.required],
+            cityNames: [null, Validators.required],
+            description: [null, Validators.required],
+            active: [false],
+        });
     }
 
-    const allowedTypes = ['image/jpeg', 'image/png'];
-    const file = fileList[0];
-
-    // Return if the file is not allowed
-    if (!allowedTypes.includes(file.type)) {
-      return;
+    addChip(event: any): void {
+        const input = event.input;
+        const value = event.value;
+        if ((value || '').trim()) {
+            this.airportChips.push(value.trim());
+        }
+        if (input) {
+            input.value = '';
+        }
+        this.form.get('airportCode').setValue(this.airportChips);
+    }
+    addCityNames(event: any): void {
+        const input = event.input;
+        const value = event.value;
+        if ((value || '').trim()) {
+            this.CityNames.push(value.trim());
+        }
+        if (input) {
+            input.value = '';
+        }
+        this.form.get('cityNames').setValue(this.CityNames);
     }
 
-    const reader = new FileReader();
-
-    reader.onload = (event) => {
-      const fileString = event.target.result as string;
-      const base64Image = fileString.split(',')[1];
-      
-      // Create the avatar object
-      const avatar = {
-        content: base64Image,
-        name: file.name,
-        type: file.type
-      };
-
-      // Set the avatar object in the form control
-      this.form.get('avatar').setValue(avatar, { emitEvent: false }); // Don't emit the change event
-
-      // Use setTimeout to ensure the setValue operation is completed before calling createDestination
-      setTimeout(() => {
-        this.createDestination();
-      }, 0);
-    };
-
-    reader.readAsDataURL(file);
-  }
-
-  removeAvatar(): void {
-    // Get the form control for 'avatar'
-    const avatarFormControl = this.form.get('avatar');
-
-    // Clear the selected image and base64 string
-    avatarFormControl.setValue(null, { emitEvent: false }); // Don't emit the change event
-    this._avatarFileInput.nativeElement.value = null;
-  }
-
-  createDestination(): void {
-    this.form.markAllAsTouched();
-    if (this.form.valid) {
-      // Get the avatar object from the form control
-      const avatar = this.form.get('avatar').value;
-
-      // Create the destination data including the avatar
-      const destinationData = {
-        name: this.form.get('name').value,
-        index: this.form.get('index').value,
-        airport: this.form.get('airport').value,
-        description: this.form.get('description').value,
-        active: this.form.get('active').value,
-        avatar: avatar
-      };
-
-      this.destinationService.insertDeals(destinationData).subscribe({
-        next: (res: any) => {
-          console.log(res);
-          this._dialogRef.close(res.data);
-        },
-      });
+    editChip(index: number, newValue: string): void {
+        if (newValue && !this.airportChips.includes(newValue)) {
+            this.airportChips[index] = newValue;
+        }
     }
-  }
+    editCityNames(index: number, newValue: string): void {
+        if (newValue && !this.CityNames.includes(newValue)) {
+            this.CityNames[index] = newValue;
+        }
+    }
+
+    removeChip(index: number): void {
+        this.airportChips.splice(index, 1);
+    }
+    removeCityNames(index: number): void {
+        this.CityNames.splice(index, 1);
+    }
+
+    uploadAvatar(fileList: FileList): void {
+        // Return if canceled
+        if (!fileList.length) {
+            return;
+        }
+
+        const allowedTypes = ['image/jpeg', 'image/png'];
+        const file = fileList[0];
+
+        // Return if the file is not allowed
+        if (!allowedTypes.includes(file.type)) {
+            return;
+        }
+
+        const reader = new FileReader();
+
+        reader.onload = (event) => {
+            const fileString = event.target.result as string;
+            const base64Image = fileString.split(',')[1];
+
+            // Create the avatar object
+            const avatar = {
+                content: base64Image,
+                name: file.name,
+                type: file.type,
+            };
+
+            // Set the avatar object in the form control
+            this.form.get('avatar').setValue(avatar, { emitEvent: false }); // Don't emit the change event
+
+            // Use setTimeout to ensure the setValue operation is completed before calling createDestination
+            setTimeout(() => {
+                this.createDestination();
+            }, 0);
+        };
+
+        reader.readAsDataURL(file);
+    }
+
+    removeAvatar(): void {
+        // Get the form control for 'avatar'
+        const avatarFormControl = this.form.get('avatar');
+
+        // Clear the selected image and base64 string
+        avatarFormControl.setValue(null, { emitEvent: false }); // Don't emit the change event
+        this._avatarFileInput.nativeElement.value = null;
+    }
+
+    createDestination(): void {
+        this.form.markAllAsTouched();
+        if (this.form.valid) {
+            // Get the avatar object from the form control
+            const avatar = this.form.get('avatar').value;
+
+            // Create the destination data including the avatar
+            const destinationData = {
+                name: this.form.get('name').value,
+                destinationId: this.form.get('destinationId').value,
+                hotelStar: this.form.get('hotelStar').value,
+                index: this.form.get('index').value,
+                airport: this.form.get('airport').value,
+                airportCode: this.form.get('airportCode').value,
+                cityNames: this.form.get('cityNames').value,
+                description: this.form.get('description').value,
+                active: this.form.get('active').value,
+                avatar: avatar,
+            };
+
+            this.destinationService.insertDeals(destinationData).subscribe({
+                next: (res: any) => {
+                    console.log(res);
+                    this._dialogRef.close(res.data);
+                },
+            });
+        }
+    }
 }
--- src/app/modules/admin/deals/destination/destination.ts
+++ src/app/modules/admin/deals/destination/destination.ts
@@ -1,6 +1,8 @@
 
 export interface Destination
 {
+    airportCode: string[];
+    cityNames: string[];
     id: string;
     name: string;
     index: string;
