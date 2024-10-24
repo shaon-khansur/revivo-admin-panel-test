1484f36ca28b74a4baef164a3898c732b32501de
shaonkhansur <shaon@npcoding.com>
2024-10-24 18:30:56 +0600

    initial

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -16,7 +16,11 @@ export const environment = {
         measurementId: "G-LFS1NEL6Y8",
       },
 
-    // firebase: {
+
+};
+
+
+// firebase: {
     //     projectId: 'revivo-5afd9',
     //     appId: '1:561970817375:web:f74dd47bac20e041aeaeea',
     //     databaseURL: 'https://revivo-5afd9-default-rtdb.firebaseio.com',
@@ -27,4 +31,3 @@ export const environment = {
     //     messagingSenderId: '561970817375',
     //     measurementId: 'G-7RLMVYDNEG',
     // },
-};
