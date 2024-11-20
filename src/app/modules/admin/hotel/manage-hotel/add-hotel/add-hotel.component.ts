92353478aaba40bb5cb6aef4d158e733ee6428c7
Md. Asif Ullah Sikder <167691633+devAsif23@users.noreply.github.com>
2024-11-20 13:43:05 +0600

    add hotel additional info

--- src/app/modules/admin/hotel/manage-hotel/add-hotel/add-hotel.component.ts
+++ src/app/modules/admin/hotel/manage-hotel/add-hotel/add-hotel.component.ts
@@ -13,6 +13,11 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSelectModule } from '@angular/material/select';
+import { EditHotelImageComponent } from '../hotel-details/edit-hotel-image/edit-hotel-image.component';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { EditFacilityComponent } from '../hotel-details/edit-facility/edit-facility.component';
 
 @Component({
     selector: 'app-add-hotel',
@@ -26,6 +31,9 @@ import { MatSelectModule } from '@angular/material/select';
         MatButtonModule,
         MatCheckboxModule,
         MatSelectModule,
+        MatExpansionModule,
+        MatDialogModule,
+        MatIconModule,
     ],
     templateUrl: './add-hotel.component.html',
     styleUrls: ['./add-hotel.component.scss'],
@@ -35,7 +43,7 @@ export class AddHotelComponent implements OnInit {
     currentTabIndex = 0;
     ratings: number[] = [1, 2, 3, 4, 5];
 
-    constructor(private fb: FormBuilder) {}
+    constructor(private fb: FormBuilder, private dialog: MatDialog) {}
 
     ngOnInit(): void {
         this.hotelForm = this.fb.group({
@@ -69,55 +77,127 @@ export class AddHotelComponent implements OnInit {
         });
     }
 
-    get facilities(): FormArray {
-        return this.hotelForm.get('HotelFacilities') as FormArray;
-    }
-
+    // -------------------------------------------  image ---------------------------------------
     get images(): FormArray {
         return this.hotelForm.get('HotelImages') as FormArray;
     }
+
+    openEditHotelImage(index: number): void {
+        const imageControl = this.images.at(index);
+
+        const dialogRef = this.dialog.open(EditHotelImageComponent, {
+            width: '400px',
+            data: {
+                ImageTitle: imageControl.get('ImageTitle').value,
+                Url: imageControl.get('Url').value,
+                ImageType: imageControl.get('ImageType').value,
+            },
+        });
+
+        dialogRef.afterClosed().subscribe((result) => {
+            if (result?.action === 'save' && result?.data) {
+                this.images.at(index).patchValue(result.data);
+            } else if (result?.action === 'delete') {
+                this.removeImage(index);
+            }
+        });
+    }
+
+    addImage(): void {
+        const dialogRef = this.dialog.open(EditHotelImageComponent, {
+            width: '400px',
+            data: {
+                ImageTitle: '',
+                Url: '',
+                ImageType: '',
+            },
+        });
+
+        dialogRef.afterClosed().subscribe((result) => {
+            if (result?.action === 'save' && result?.data) {
+                this.images.push(this.fb.group(result.data));
+                console.log('images', this.images);
+            }
+        });
+    }
+
+    removeImage(index: number): void {
+        if (this.images.length >= 0) {
+            this.images.removeAt(index);
+        }
+    }
+
+    // -------------------------------------------  facility ---------------------------------------
+    get facilities(): FormArray {
+        return this.hotelForm.get('HotelFacilities') as FormArray;
+    }
+    openEditFacility(index: number): void {
+        const facilityControl = this.facilities.at(index);
+
+        const dialogRef = this.dialog.open(EditFacilityComponent, {
+            width: '400px',
+            data: {
+                FacilityTitle: facilityControl.get('FacilityTitle').value,
+                FacilityCode: facilityControl.get('FacilityCode').value,
+                FacilityType: facilityControl.get('FacilityType').value,
+                Url: facilityControl.get('Url').value,
+            },
+        });
+
+        dialogRef.afterClosed().subscribe((result) => {
+            if (result?.action === 'save' && result?.data) {
+                this.facilities.at(index).patchValue(result.data);
+            } else if (result?.action === 'delete') {
+                this.removeFacility(index);
+            }
+        });
+    }
+
+    addFacility(): void {
+        const dialogRef = this.dialog.open(EditFacilityComponent, {
+            width: '400px',
+            data: {
+                FacilityTitle: '',
+                FacilityCode: '',
+                FacilityType: '',
+                Url: '',
+            },
+        });
+
+        dialogRef.afterClosed().subscribe((result) => {
+            if (result?.action === 'save' && result?.data) {
+                this.facilities.push(this.fb.group(result.data));
+            }
+        });
+    }
+
+    removeFacility(index: number): void {
+        if (this.facilities.length >= 0) {
+            this.facilities.removeAt(index);
+        }
+    }
+
+    // -------------------------------------------  Remark ---------------------------------------
 
     get remarks(): FormArray {
         return this.hotelForm.get('HotelRemarks') as FormArray;
     }
 
-    addFacility(): void {
-        this.facilities.push(
-            this.fb.group({
-                FacilityTitle: ['', Validators.required],
-                Url: [''],
-            })
-        );
-    }
-
-    removeFacility(index: number): void {
-        this.facilities.removeAt(index);
-    }
-
-    addImage(): void {
-        this.images.push(
-            this.fb.group({
-                ImageTitle: ['', Validators.required],
-                Url: ['', Validators.required],
-            })
-        );
-    }
-
-    removeImage(index: number): void {
-        this.images.removeAt(index);
+    private createHotelRemarkGroup(): FormGroup {
+        return this.fb.group({
+            FreeText: [''],
+            RemarkType: [''],
+        });
     }
 
     addRemark(): void {
-        this.remarks.push(
-            this.fb.group({
-                RemarkType: ['', Validators.required],
-                FreeText: ['', Validators.required],
-            })
-        );
+        this.remarks.push(this.createHotelRemarkGroup());
     }
 
     removeRemark(index: number): void {
-        this.remarks.removeAt(index);
+        if (this.remarks.length >= 0) {
+            this.remarks.removeAt(index);
+        }
     }
 
     goToNextTab(): void {
